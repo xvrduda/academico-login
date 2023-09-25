7a2004101169aabc178faa8300d4589001953e37
Maria Eduarda Xavier <rm89430@estudante.fieb.edu.br>
2023-09-25 20:32:50 -0300

    Em processo de construção do perfil

--- src/main/java/com/itb/mif3an/academicologin/web/UserController.java
+++ src/main/java/com/itb/mif3an/academicologin/web/UserController.java
@@ -3,10 +3,13 @@ package com.itb.mif3an.academicologin.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.itb.mif3an.academicologin.model.User;
 import com.itb.mif3an.academicologin.service.UserService;
+import com.itb.mif3an.academicologin.web.dto.UserDto;
 
 @Controller
 public class UserController {
@@ -27,6 +30,17 @@ public class UserController {
 		String username = user.getEmail();
 		model.addAttribute("username", username);
 		
-		return "home";
+		return home;
+	}
+	
+	@GetMapping("/users/perfil/{username}")
+	public String showPerfilForm(@PathVariable("username")String username, ModelMap model) {
+		
+		UserDto userDto = new UserDto();
+		userDto.setEmail(username);
+		User user = userService.findByEmail(userDto.getEmail());
+		model.addAttribute("user", user);
+		
+		return "update-registration";
 	}
 }
